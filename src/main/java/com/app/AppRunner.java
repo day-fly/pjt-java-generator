4c92ff5ffe4b90da2f45d8f188587e8daa4b1414
hyungoo kim <sweet.khg@gmail.com>
2020-03-01 21:06:58 +0900

    add spring jdbc

--- src/main/java/com/app/AppRunner.java
+++ src/main/java/com/app/AppRunner.java
@@ -5,8 +5,12 @@ import com.app.service.SourceMaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,6 +19,13 @@ public class AppRunner implements ApplicationRunner {
 
     @Autowired
     SourceMaker sourceMaker;
+
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -33,5 +44,22 @@ public class AppRunner implements ApplicationRunner {
 
 //        System.out.println(globalValue.get());
         sourceMaker.makeController();
+
+        try(Connection connection = dataSource.getConnection()){
+            System.out.println(connection);
+            String URL = connection.getMetaData().getURL();
+            System.out.println(URL);
+            String User = connection.getMetaData().getUserName();
+            System.out.println(User);
+
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE ACCOUNT(" +
+                    "ID INTEGER NOT NULL," +
+                    "NAME VARCHAR(255)," +
+                    "PRIMARY KEY(ID))";
+            statement.executeUpdate(sql);
+        }
+
+        jdbcTemplate.execute("INSERT INTO ACCOUNT VALUES(1, 'saelobi')");
     }
 }
