30374597e8b029e33a08c6d5c2dc51f9f4ca7ee6
hyungoo kim <sweet.khg@gmail.com>
2020-03-18 00:28:37 +0900

    'working..'

--- src/main/java/com/app/service/generator/MapperXML.java
+++ src/main/java/com/app/service/generator/MapperXML.java
@@ -30,80 +30,87 @@ public class MapperXML {
         xmlCode += "    SELECT\n";
 
         final String[] columnStr = {""};
+        final String[] where = {""};
         columns.forEach(v -> {
-            if(columns.indexOf(v)  == 0) {
+            String camelCaseColumnName = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, v.getColumnName().toLowerCase());
+            camelCaseColumnName = camelCaseColumnName.substring(0, 1).toLowerCase() + camelCaseColumnName.substring(1);
+            if ("Y".equals(v.getIsPk())) {
+                where[0] += "      AND " + v.getColumnName().toUpperCase() + " = #{" + requestDto.getFirstLowerCaseFilePrefix() + ConstValue.SEARCH_VO + "." + camelCaseColumnName + "}\n";
+            }
+
+            if (columns.indexOf(v) == 0) {
                 columnStr[0] += "        " + v.getColumnName().toUpperCase() + "\n";
-            }else{
+            } else {
                 columnStr[0] += "       ," + v.getColumnName().toUpperCase() + "\n";
             }
         });
 
         xmlCode += columnStr[0];
         xmlCode += "    FROM " + requestDto.getTableName().toUpperCase() + "\n";
-        xmlCode += "<select/>\n\n";
+        xmlCode += "</select>\n\n";
 
 
         //select query - get
         xmlCode += "<select id=\"get\" resultType=\"" + voPath + "\">\n";
         xmlCode += "    SELECT\n";
 
-        final String[] columnStr2 = {""};
-        final String[] where = {""};
-        columns.forEach(v -> {
-            String camelCaseColumnName = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, v.getColumnName().toLowerCase());
-            camelCaseColumnName = camelCaseColumnName.substring(0, 1).toLowerCase() + camelCaseColumnName.substring(1);
-            if("Y".equals(v.getIsPk())){
-                where[0] += "      AND " + v.getColumnName().toUpperCase() + " = #{" + requestDto.getFirstLowerCaseFilePrefix() + ConstValue.SEARCH_VO + "." + camelCaseColumnName + "}\n";
-            }
-            if(columns.indexOf(v)  == 0) {
-                columnStr2[0] += "        " + v.getColumnName().toUpperCase() + "\n";
-            }else{
-                columnStr2[0] += "       ," + v.getColumnName().toUpperCase() + "\n";
-            }
-        });
-
-        xmlCode += columnStr2[0];
+        xmlCode += columnStr[0];
         xmlCode += "    FROM " + requestDto.getTableName().toUpperCase() + "\n";
         xmlCode += "    WHERE 1=1\n";
         xmlCode += where[0];
-        xmlCode += "<select/>\n\n";
+        xmlCode += "</select>\n\n";
 
 
+        //inesrt query
+        xmlCode += "<insert id=\"create\">\n";
+        xmlCode += "    INSERT INTO " + requestDto.getTableName().toUpperCase() + "( \n";
+        xmlCode += columnStr[0];
+        xmlCode += "    )\n";
+        xmlCode += "    VALUES (\n";
 
-//
-//
-//
-//        List<FieldSpec> fieldSpecList = new ArrayList<>();
-//        columns.forEach(v -> {
-//            String className = "";
-//            List<AnnotationSpec> annotationSpecs = new ArrayList<>();
-//
-//            switch (v.getUdtName()) {
-//                case ("int4"):
-//                    className = "Integer";
-//                    break;
-//                default:
-//                    className = "String";
-//                    annotationSpecs.add(
-//                            AnnotationSpec.builder(Size.class)
-//                                    .addMember("max", "$S", v.getCharacterMaximumLength())
-//                                    .build()
-//                    );
-//            }
-//            if ("NO".equals(v.getIsNullable())) {
-//                annotationSpecs.add(
-//                        AnnotationSpec.builder(NotEmpty.class).build()
-//                );
-//            }
-//            fieldSpecList.add(
-//                    FieldSpec.builder(ClassName.bestGuess(className),
-//                            v.getColumnName(), Modifier.PRIVATE)
-//                            .addAnnotations(annotationSpecs)
-//                            .build()
-//            );
-//        });
+        final String[] columnStr2 = {""};
+        columns.forEach(v -> {
+            String camelCaseColumnName = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, v.getColumnName().toLowerCase());
+            camelCaseColumnName = camelCaseColumnName.substring(0, 1).toLowerCase() + camelCaseColumnName.substring(1);
+            if (columns.indexOf(v) == 0) {
+                columnStr2[0] += "        #{" + requestDto.getFirstLowerCaseFilePrefix() + ConstValue.VO + "." + camelCaseColumnName + "}\n";
+            } else {
+                columnStr2[0] += "       ,#{" + requestDto.getFirstLowerCaseFilePrefix() + ConstValue.VO + "." + camelCaseColumnName + "}\n";
+            }
+        });
+        xmlCode += columnStr2[0];
+        xmlCode += "    )\n";
+        xmlCode += "</insert>\n\n";
 
+
+        //update query
+        xmlCode += "<update id=\"update\">\n";
+        xmlCode += "    UPDATE " + requestDto.getTableName().toUpperCase() + "\n";
+        xmlCode += "    SET \n";
+
+        final String[] columnStr3 = {""};
+        int[] i = {0};
+        columns.stream().filter(f -> "N".equals(f.getIsPk())).forEach(v -> {
+            String camelCaseColumnName = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, v.getColumnName().toLowerCase());
+            camelCaseColumnName = camelCaseColumnName.substring(0, 1).toLowerCase() + camelCaseColumnName.substring(1);
+
+            if (i[0]++ == 0) {
+                columnStr3[0] += "        " + v.getColumnName().toUpperCase() + " = #{" + requestDto.getFirstLowerCaseFilePrefix() + ConstValue.VO + "." + camelCaseColumnName + "}\n";
+            } else {
+                columnStr3[0] += "       ," + v.getColumnName().toUpperCase() + " = #{" + requestDto.getFirstLowerCaseFilePrefix() + ConstValue.VO + "." + camelCaseColumnName + "}\n";
+            }
+        });
+        xmlCode += columnStr3[0];
+        xmlCode += "    WHERE 1=1\n";
+        xmlCode += where[0].replaceAll(ConstValue.SEARCH_VO, ConstValue.VO);
+        xmlCode += "</update>\n\n";
+
+        xmlCode += "<delete id=\"delete\">\n";
+        xmlCode += "    DELETE FROM " + requestDto.getTableName().toUpperCase() + "\n";
+        xmlCode += "    WHERE 1=1\n";
+        xmlCode += where[0].replaceAll(ConstValue.SEARCH_VO, ConstValue.VO);
+        xmlCode += "</delete>\n\n";
         xmlCode += "</mapper>";
-       return xmlCode;
+        return xmlCode;
     }
 }
